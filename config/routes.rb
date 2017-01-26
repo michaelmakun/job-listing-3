de6c14f978c0558af36b900fad4ca4c0e12b7875
makun <michaelmakun@aliyun.com>
2017-01-27 00:08:10 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
 
-  resources :jobs do
-    resources :resumes
-  end
-
-  namespace :admin do
     resources :jobs do
-      member do
-        post :publish
-        post :hide
-      end
-
-      resources :resumes
+        resources :resumes
     end
-  end
 
-  root 'jobs#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :admin do
+        resources :jobs do
+            member do
+                post :publish
+                post :hide
+            end
+
+            resources :resumes
+        end
+    end
+
+    root 'welcome#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
