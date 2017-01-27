a17cf1590a9bdd851d069cc789e0dd3a6d28116a
makun <michaelmakun@aliyun.com>
2017-01-27 21:18:34 +0800

    implement will_paginate

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -19,6 +19,7 @@ class JobsController < ApplicationController
             else
               Job.published.recent
             end
+    @jobs = @jobs.paginate(:page => params[:page], :per_page => 10)
   end
 
   def new
